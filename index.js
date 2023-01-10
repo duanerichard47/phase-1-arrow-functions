6cec4f3631b313860403f898150ccc20f299c20d
Duane <duanerichard47@hotmail.com>
2023-01-10 11:39:47 -0500

    complete arrow function lab

---
+++ index.js
@@ -0,0 +1,13 @@
+let a = 2000;
+let b = 100;
+function divide( num1,numb2){
+   return num1/numb2;
+}
+
+/*function square( num1){
+    return num1*num1;
+ }  */
+
+const square = (num1) =>num1 * num1;
+
+const add = (num1,num2) => num1 + num2;
